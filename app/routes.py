22d9bd7cd5fcb77eddd6c22884d7a94261ad9644
olestrausss <o@olestrauss.de>
2024-01-20 23:17:20 -0500

    added psql integration

--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,68 @@
 from app import app
 from flask import request, jsonify
+from flask_cors import CORS, cross_origin
 from werkzeug.utils import secure_filename
+import psycopg2
 import os
+
+CORS(app, resources={r'/api/*': {'origins': 'http://localhost:3000'}})
+
+conn = psycopg2.connect(os.getenv('POSTGRES_URI'))
 
 @app.route('/')
 def index():
     return "Welcome to Soundscape!"
 
 @app.route('/upload-song', methods=['POST'])
-def upload_song():
-    if 'file' not in request.files:
-        return 'No file part', 400
-    file = request.files['file']
-    if file.filename == '':
-        return 'No selected file', 400
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return 'File uploaded', 200
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in {'mp3', 'wav'}
-
-@app.route('/add-song', methods=['POST'])
 def add_song():
-    # Extract song details from request
-    # Save song details to database
-    return 'Song added', 200
+    try:
+        # Extract song details from request
+        title = request.form['title']
+        artist = request.form['artist']
+        cover_image_url = request.form['cover_image_url']
+
+        # Insert song into the database
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO songs (title, artist, cover_image_url) VALUES (%s, %s, %s)",
+            (title, artist, cover_image_url)
+        )
+        conn.commit()
+        cursor.close()
+
+        return 'Song added', 200
+
+    except Exception as e:
+        return str(e), 500
+
 
 @app.route('/api/songs', methods=['GET'])
+@cross_origin()
 def get_songs():
-    songs = [
-        {"id": 1, "title": "Song One", "artist": "Artist A", "coverImageUrl": "url_to_cover_image_1"},
-        {"id": 2, "title": "Song Two", "artist": "Artist B", "coverImageUrl": "url_to_cover_image_2"},
-        # ... more songs
-    ]
-    return jsonify(songs)
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, title, artist, cover_image_url FROM songs")
+        songs = cursor.fetchall()
+        cursor.close()
+
+        songs_data = []
+        for song in songs:
+            songs_data.append({
+                'id': song[0],
+                'title': song[1],
+                'artist': song[2],
+                'cover_image_url': song[3]
+            })
+
+        return jsonify(songs_data)
+
+    except Exception as e:
+        return str(e), 500
+    
+'''
+@app.teardown_appcontext
+def close_db(error):
+    conn.close()
+'''
 
 
