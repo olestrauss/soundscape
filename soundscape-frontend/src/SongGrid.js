f2f04386b36a7086dd56e9c3d0f798c8e4e2dc40
olestrausss <o@olestrauss.de>
2024-01-21 17:04:05 -0500

    Changed layout of grid

--- soundscape-frontend/src/SongGrid.js
+++ soundscape-frontend/src/SongGrid.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import './SongGrid.css';
-import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 
 const IMAGE_CLASS = 'song-image';
 
-const SongGrid = ({ songs, onDelete }) => {
+
+
+const SongGrid = ({ songs, onDelete, onSongSelect }) => {
   return (
     <div className="song-grid">
       {songs.map(song => (
         <div key={song.id} className="song">
-          {/* Construct the relative path for the cover image */}
           <img 
             src={`http://localhost:5000/api/media/covers/${song.id}`}
             alt={song.title} 
+            onClick={() => onSongSelect(`http://localhost:5000/api/media/audio/${song.id}`)}
             className={IMAGE_CLASS} 
-          />
+          /> 
           <h3>{song.title}</h3>
           <p>{song.artist}</p>
-          {/* Construct the relative path for the audio file */}
-          <AudioPlayer
-            src={`http://localhost:5000/api/media/audio/${song.id}`}
-            onPlay={e => console.log("onPlay")}
-            // Additional audio player props here
-          />
-          <button onClick={() => onDelete(song.id)} className="delete-button">🗑️</button>
+          <button onClick={() => onDelete(song.id)} className="delete-button">🗑️</button>     
+          {/* Rest of the song details */}
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default SongGrid;
