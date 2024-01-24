c6178e330dd6c679a1607287700f0be6de107766
olestrausss <o@olestrauss.de>
2024-01-23 22:56:50 -0500

    navbar styling and position

--- soundscape-frontend/src/NavBar.js
+++ soundscape-frontend/src/NavBar.js
@@ -1,17 +1,22 @@
 // NavBar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './navBar.css';
+import './NavBar.css';
 
 function NavBar() {
   return (
     <nav className="navbar">
       <ul className="nav-tabs">
         <li className="nav-tab">
+          <Link to="/">
+            <img src="logo_no_bg.png" alt="Logo" width="150" height="22"/>
+          </Link>
+        </li>
+        <li className="nav-tab">
           <Link to="/">Library</Link>
         </li>
         <li className="nav-tab">
-          <Link to="/upload-form">Upload Song</Link>
+          <Link to="/upload-form">Upload</Link>
         </li>
       </ul>
     </nav>
