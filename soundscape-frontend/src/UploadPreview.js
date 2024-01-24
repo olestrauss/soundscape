3cd9d1dde2dd885ebb541d9c678c1b0d93d53dc9
olestrausss <o@olestrauss.de>
2024-01-23 22:57:01 -0500

    fixed preview and styling

--- soundscape-frontend/src/UploadPreview.js
+++ soundscape-frontend/src/UploadPreview.js
@@ -1,15 +1,16 @@
 // UploadPreview.js
 
 import React from 'react';
+import './UploadPreview.css';
 
 const SongPreview = ({ title, artist, coverImageUrl }) => {
   return (
-    <div className="song-card">
-      <div className="song-image-container">
-        <img src={coverImageUrl} alt="" className="song-image" />
+    <div className="preview-song-card">
+      <div className="preview-song-image-container">
+        <img src={coverImageUrl} alt="" className="preview-song-image" />
       </div>
-      <h3 className="song-title">{title}</h3>
-      <p className="song-info">{artist}</p>
+      <h3 className="preview-song-title">{title}</h3>
+      <p className="preview-song-info">{artist}</p>
     </div>
   );
 };
