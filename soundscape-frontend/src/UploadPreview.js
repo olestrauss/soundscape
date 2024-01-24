db7315df57dfc9febab29f02ac2ef04dfdbe6c3b
olestrausss <o@olestrauss.de>
2024-01-24 00:24:32 -0500

    Refactor UploadPreview component styles

--- soundscape-frontend/src/UploadPreview.js
+++ soundscape-frontend/src/UploadPreview.js
@@ -3,11 +3,15 @@
 import React from 'react';
 import './UploadPreview.css';
 
+
 const SongPreview = ({ title, artist, coverImageUrl }) => {
+
+  const imageSource = coverImageUrl || "logo_speaker.png";
+
   return (
     <div className="preview-song-card">
       <div className="preview-song-image-container">
-        <img src={coverImageUrl} alt="" className="preview-song-image" />
+        <img src={imageSource} alt="" className="preview-song-image" />
       </div>
       <h3 className="preview-song-title">{title}</h3>
       <p className="preview-song-info">{artist}</p>
